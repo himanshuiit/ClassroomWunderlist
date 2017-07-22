8475e884d320609ef6479b096bb78107616d79ea
madHEYsia <submiitr07@gmail.com>
2017-07-22 18:58:14 +0530

    lib addition, .iml changes

---
+++ src/com/ClassroomWunderlist/database/closeWIndow/setCloseCheck.java
@@ -0,0 +1,27 @@
+package com.ClassroomWunderlist.database.closeWIndow;
+
+import com.ClassroomWunderlist.database.utils.DBUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class setCloseCheck {
+
+    public static void setCloseCheck(){
+//        Connection conn = null;
+//        PreparedStatement stmtt = null;
+//
+//        try {
+//            conn = DBUtils.getConnection();
+//            stmtt = conn.prepareStatement("UPDATE `prime`.`defaults` SET `link`='1' WHERE `parameter name`='close check';");
+//            stmtt.executeUpdate();
+//        } catch (SQLException sql) {
+//            sql.printStackTrace();
+//        } finally {
+//            DBUtils.closeStatement(stmtt);
+//            DBUtils.closeConnection(conn);
+//        }
+    }
+
+}
