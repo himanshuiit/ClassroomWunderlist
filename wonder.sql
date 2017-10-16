cb076496e0a9b3d8f58306bb6f764f363e2bab6f
himanshuiit <himanshusinghal9298@gmail.com>
2017-10-16 12:49:48 +0530

    Add files via upload

---
+++ wonder.sql
@@ -0,0 +1,66 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.19-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table newwunderlist.bugcomment: ~0 rows (approximately)
+DELETE FROM `bugcomment`;
+/*!40000 ALTER TABLE `bugcomment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bugcomment` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.buginlist: ~0 rows (approximately)
+DELETE FROM `buginlist`;
+/*!40000 ALTER TABLE `buginlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buginlist` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.bugsubtask: ~0 rows (approximately)
+DELETE FROM `bugsubtask`;
+/*!40000 ALTER TABLE `bugsubtask` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bugsubtask` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.company: ~0 rows (approximately)
+DELETE FROM `company`;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.companyuser: ~0 rows (approximately)
+DELETE FROM `companyuser`;
+/*!40000 ALTER TABLE `companyuser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `companyuser` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.conversation: ~0 rows (approximately)
+DELETE FROM `conversation`;
+/*!40000 ALTER TABLE `conversation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversation` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.currentuser: ~0 rows (approximately)
+DELETE FROM `currentuser`;
+/*!40000 ALTER TABLE `currentuser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `currentuser` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.list: ~0 rows (approximately)
+DELETE FROM `list`;
+/*!40000 ALTER TABLE `list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `list` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.messages: ~0 rows (approximately)
+DELETE FROM `messages`;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+
+-- Dumping data for table newwunderlist.user: ~0 rows (approximately)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
